ee06d9a3a92ae587b9eccd1142802057f047b59a
Eva Garcia <C8VMT@ferrovial.com>
2025-08-20 21:22:32 +0200

    feat: add more features

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // ¡IMPORTANTE!
-  // Actualiza estos valores con tu usuario y el nombre de tu repositorio de GitHub
   site: 'https://eva.gtorre.github.io',
   base: '/MyBirthday',
 });
