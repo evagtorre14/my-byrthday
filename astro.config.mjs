922dfec37097444fe75fbb5a523e0c7a6d5b4fdd
Eva Garcia <C8VMT@ferrovial.com>
2025-08-20 20:09:16 +0200

    feat: add github actions configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  // ¡IMPORTANTE!
+  // Actualiza estos valores con tu usuario y el nombre de tu repositorio de GitHub
+  site: 'https://eva.gtorre.github.io',
+  base: '/my-birthday',
 });
